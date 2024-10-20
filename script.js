86aa94dc2ca5d41b2076beb866dfdfdcd14c50e6
Barmington Chuku <barmingtonk@gmail.com>
2024-10-20 21:41:38 +0100

    complete cookie clicker

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const cookieBtn = document.getElementById("cookie-btn");
+const main = document.querySelector(".main-container");
 const cookieDisplay = document.getElementById("cookie-display");
 const cpsDisplay = document.getElementById("cps-display");
-const autoClicker = document.getElementById("auto_clicker");
 const upgradeBtn = document.getElementById("upgr-Btn");
-const upgradeContainer = document.querySelector("div");
+const upgradeContainer = document.querySelector(".grid-container");
 const upgradeNameDisplay = document.getElementById("upgrade-name");
 const levelTopRight = document.querySelector(".levels");
-// const nextUpgrNotification = document.querySelector("nextUpgrAlert");
-
-console.log(nextUpgrNotification);
+const topLevel = document.querySelector(".item36");
+const hideBtn = document.querySelector("hideBtn");
+const hideCookiesTest = document.querySelector(".hideP");
+const newBtn = document.createElement("button");
+let newh2 = document.createElement("h2");
 
 let cookies = Number(localStorage.getItem("cookies")) || 0;
 let cps = Number(localStorage.getItem("cps"));
 let counter = 0;
 let upgrades;
-//Fetch api
+
 async function fetchData() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
@@ -28,20 +30,23 @@ let intervalId = setInterval(function () {
       cookies = cookies + cps;
       cookieDisplay.textContent = cookies;
     } else {
+      newh2.textContent = `Wohoo! You made it to the top 🎉!`;
+      topLevel.append(newh2);
+      upgradeBtn.classList.add("hide");
+      main.style.backgroundImage =
+        "linear-gradient(to top left, #753682 0%, #bf2e34 100%)";
+      main.style.color = "white";
+
       clearInterval(intervalId);
     }
   }
-}, 100);
+}, 1000);
 
 cookieBtn.addEventListener("click", () => {
   cookies++;
   cookieDisplay.textContent = cookies;
   localStorage.setItem("cookies", cookies);
-  localStorage.clear("cookies");
 });
-
-const newBtn = document.createElement("button");
-let newh2 = document.createElement("h2");
 
 upgradeBtn.addEventListener("click", () => {
   if (cookies >= upgrades[counter].cost) {
@@ -49,30 +54,15 @@ upgradeBtn.addEventListener("click", () => {
     cps = cps + upgrades[counter].increase;
     cpsDisplay.textContent = cps;
     localStorage.setItem("cps", cps);
-    localStorage.clear("cps");
-
     newh2.textContent = `Upgrade level (${upgrades[counter].id}) - ${upgrades[counter].name}`;
-    upgradeContainer.append(newh2);
+    upgradeNameDisplay.textContent = newh2.innerHTML;
   }
+
   for (let i = 0; i < upgrades.length; i++) {
     levelTopRight.textContent = `(level ${counter + 1} of 10)`;
   }
+
   counter++;
 });
-
-// newBtn.addEventListener("click", () => {
-//   if (cookies >= upgrades[counter].cost) {
-//     cookies = cookies - upgrades[counter].cost;
-//     cps = cps + upgrades[counter].increase;
-
-//     cpsDisplay.textContent = cps;
-//     newh2 = document.createElement("h2");
-//     newh2.textContent += `${upgrades[counter].name}: costs ${upgrades[counter].cost} cookies, click the button below to buy.`;
-//     upgradeContainer.append(newh2);
-//     upgradeContainer.append(newBtn);
-
-//     counter++;
-//   }
-// });
 
 fetchData();
